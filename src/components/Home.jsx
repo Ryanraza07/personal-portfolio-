6217e533e92e3c01dab05f8de66ec61cec78eb6c
Ryanraza07 <raazaltaf009@gmail.com>
2025-01-06 15:24:17 +0530

    navbar is completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+function Home(){
+    return(
+      <div></div>
+    );
+}
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+// Navbar.jsx
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand ms-5"  href="#" style={{fontSize:'2rem'}}> Portfolio</a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto me-5">
+                        <li className="nav-item mx-3">
+                            <a className="nav-link active hover-effect" href="#">Home</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link" href="#">Services</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
