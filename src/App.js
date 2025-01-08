d74f23af19ad2fb4677ebd69c629689e0496caaa
Ryanraza07 <raazaltaf009@gmail.com>
2025-01-08 15:00:02 +0530

    The skills Section is Completed

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import react from 'react';
 import Navbar from './components/Navbar'
 import Home from './components/Home';
 import Experience from './components/Experience';
+import Skills from './components/Skills';
 function App(){
   return (
   <> <Navbar />
   <Home />
   <Experience/>
+  <Skills />
   </>
 
   );
