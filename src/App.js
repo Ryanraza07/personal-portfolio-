82c9777555c1170ddc76d8ed2cf066c45beb857d
Ryanraza07 <raazaltaf009@gmail.com>
2025-01-08 17:14:27 +0530

    Project is completed

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Navbar from './components/Navbar'
 import Home from './components/Home';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 function App(){
   return (
   <> <Navbar />
   <Home />
   <Experience/>
   <Skills />
+  <Projects />
   </>
 
   );
